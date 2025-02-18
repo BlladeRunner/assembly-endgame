ee68b186840296dacdbab6f2df4ff6364e1dc8c7
Blladerunner <rufatahmedik@gmail.com>
2025-02-18 22:27:23 +0100

    Keyboard letter styles for guesses

--- Main.jsx
+++ Main.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { languages } from "./languages";
+import { clsx } from "clsx";
 
 export default function Main() {
   const [currentWord, setCurrentWord] = useState("react");
@@ -29,11 +30,22 @@ export default function Main() {
     .split("")
     .map((letter, index) => <span key={index}>{letter.toUpperCase()}</span>);
 
-  const keyboardElements = alphabet.split("").map((letter) => (
-    <button key={letter} onClick={() => addGuessedLetter(letter)}>
-      {letter.toUpperCase()}
-    </button>
-  ));
+  const keyboardElements = alphabet.split("").map((letter) => {
+    const isGuessed = guessedLetters.includes(letter);
+    const isCorrect = isGuessed && currentWord.includes(letter);
+    const isWrong = isGuessed && !currentWord.includes(letter);
+    const className = clsx({ correct: isCorrect, wrong: isWrong });
+
+    return (
+      <button
+        className={className}
+        key={letter}
+        onClick={() => addGuessedLetter(letter)}
+      >
+        {letter.toUpperCase()}
+      </button>
+    );
+  });
 
   return (
     <main>
